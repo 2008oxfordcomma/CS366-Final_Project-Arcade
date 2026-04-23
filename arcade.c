8f122864ed83e62f981eacf30d50d56de78a0e24
Dre Harm <2008oxfordcomma@gmail.com>
2026-04-23 12:02:11 -0500

    See description for changes
    Moved settings display code
    Added ability to go back to the main menu but not quit the program
    Added tic-tac-toe game

--- arcade.c
+++ arcade.c
@@ -6,6 +6,8 @@
 
 #include <ncurses.h>
 #include <string.h>
+#include <stdlib.h>
+#include "tictactoe.h"
 #include "blackjack.h"
 
 #define MENU_ITEMS 3
@@ -16,7 +18,7 @@ int main(void) {
     int choice;
     int ch;
     int row, col;
-    int start_y;
+    int startY;
 
     /* this can be added onto later if we need to, remember to change MENU_ITEMS to reflect changes */
     choices[0] = "Tic-Tac-Toe";
@@ -24,7 +26,6 @@ int main(void) {
     choices[2] = "Settings";
 
     highlight = 0;
-    choice = -1;
 
     initscr();
     cbreak();
@@ -32,80 +33,89 @@ int main(void) {
     keypad(stdscr, 1);
     curs_set(0);
 
-    while (choice == -1) {
-        clear();
-        getmaxyx(stdscr, row, col);
+    while(1) {
+        choice = -1;
 
+        while (choice == -1) {
+            clear();
+            getmaxyx(stdscr, row, col);
+
+            box(stdscr, 0, 0); /* draws the boarder */
+
+            char *title = "=== Arcade Games ===";
+            char *instructions = "Use arrow keys for navigation, 'Enter' to select, and 'q' to quit";
+            mvprintw(2, (col - strlen(title)) / 2, "%s", title);
+            mvprintw(4, (col - strlen(instructions)) / 2, "%s", instructions);
+
+            startY = row / 2 - MENU_ITEMS / 2; /* calculates the starting y position for the menu items */
+
+            int i;
+            for (i = 0; i < MENU_ITEMS; i++) { /* prints the menu items */
+                int x = (col - strlen(choices[i])) / 2;
+
+                if (i == highlight) { 
+                    attron(A_REVERSE); /* highlights the selected menu item */
+                    mvprintw(startY + i, x, "%s", choices[i]); /* prints the menu item */
+                    attroff(A_REVERSE); /* turns off the highlight for the next item */
+                } else { /* prints the non-highlighted menu items */
+                    mvprintw(startY + i, x, "%s", choices[i]);
+                }
+            }
+            refresh();
+            ch = getch();
+
+            switch (ch) {
+                case KEY_UP:
+                    if (highlight > 0) {
+                        highlight--;
+                    }
+                    break;
+                case KEY_DOWN:
+                    if (highlight < MENU_ITEMS - 1) {
+                        highlight++;
+                    }
+                    break;
+                case 10: /* docs list 2 different enter key values so I'm including both */
+                case KEY_ENTER: 
+                    choice = highlight;
+                    break;
+                case 'q': /* covering both options for 'q' */
+                case 'Q':
+                    endwin();
+                    return 0;
+           }
+        }
+    
+        clear();
         box(stdscr, 0, 0);
 
-        char *title = "=== Arcade Games ===";
-        char *instructions = "Use arrow keys for navigation, 'Enter' to select, and 'q' to quit";
-        mvprintw(2, (col - strlen(title)) / 2, "%s", title);
-        mvprintw(4, (col - strlen(instructions)) / 2, "%s", instructions);
+        char buffer[64];
 
-
-        start_y = row / 2 - MENU_ITEMS / 2;
-
-        int i;
-        for (i = 0; i < MENU_ITEMS; i++) {
-            int x = (col - strlen(choices[i])) / 2;
-
-            if (i == highlight) { 
-                attron(A_REVERSE); 
-                mvprintw(start_y + i, x, "%s", choices[i]);
-                attroff(A_REVERSE); 
-            } else {
-                mvprintw(start_y + i, x, "%s", choices[i]);
-            }
-        }
-        refresh();
-        ch = getch();
-
-        switch (ch) {
-            case KEY_UP:
-                if (highlight > 0)
-                    highlight--;
-                break;
-            case KEY_DOWN:
-                if (highlight < MENU_ITEMS - 1)
-                    highlight++;
-                break;
-            case 10: /* docs list 2 different enter key values so I'm including both */
-            case KEY_ENTER: 
-                choice = highlight;
-                break;
-            case 'q': /* covering both options for 'q' */
-            case 'Q':
-                endwin();
-                return 0;
+        /* eventually this will open and run the different game files */
+        if (choice == 0) {
+            endwin();
+            runTicTacToe();
+            initscr();
+            cbreak();
+            noecho();
+            keypad(stdscr, TRUE);
+            curs_set(0);
+        } else if (choice == 1) {
+            endwin();
+            run_blackjack();
+            initscr();
+            cbreak();
+            noecho();
+            keypad(stdscr, 1);
+            curs_set(0);
+        } else {
+            strcpy(buffer, "You selected Settings. This has not been implemented yet.");
+            mvprintw(row / 2, (col - strlen(buffer)) / 2, "%s", buffer);
+            mvprintw(row / 2 + 2, (col - 24) / 2, "Press any key to exit.");
+            refresh();
+            getch();
         }
     }
-
-    clear();
-    box(stdscr, 0, 0);
-
-    char buffer[64];
-
-    /* eventually this will open and run the different game files */
-    if (choice == 0) {
-        strcpy(buffer, "You selected Tic-Tac-Toe.");
-    } else if (choice == 1) {
-        endwin();
-        run_blackjack();
-        initscr();
-        cbreak();
-        noecho();
-        keypad(stdscr, 1);
-        curs_set(0);
-        return 0;
-    } else {
-        strcpy(buffer, "You selected Settings.");
-    }
-    mvprintw(row / 2, (col - strlen(buffer)) / 2, "%s", buffer);
-
-    mvprintw(row / 2 + 2, (col - 24) / 2, "Press any key to exit...");
-    refresh();
-    getch();
 
     endwin();
     return 0;
